2709f4ab10ef7086b608cedddf6e7f8cc2c6f30b
John Simon <john@whatisaph.one>
2019-09-25 08:42:20 -0400

    Initial commit

---
+++ crates/bin/src/main.rs
@@ -0,0 +1,8 @@
+#![warn(future_incompatible, rust_2018_compatibility, rust_2018_idioms, unused)]
+#![warn(clippy::pedantic)]
+// #![warn(clippy::cargo)]
+#![cfg_attr(feature = "strict", deny(warnings))]
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ crates/lib/src/lib.rs
@@ -0,0 +1,12 @@
+#![warn(future_incompatible, rust_2018_compatibility, rust_2018_idioms, unused)]
+#![warn(clippy::pedantic)]
+// #![warn(clippy::cargo)]
+#![cfg_attr(feature = "strict", deny(warnings))]
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
