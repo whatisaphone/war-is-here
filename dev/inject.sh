de51eb67c3580cd325a7338758aff63f08347624
John Simon <john@whatisaph.one>
2019-11-14 20:18:42 -0500

    Rename marionette to aether

--- dev/inject.sh
+++ dev/inject.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 nc localhost 53508 <<<shutdown || true
 cargo build
-vendor/Injector.exe -n darksiders1.exe -i ../target/debug/marionette.dll
+vendor/Injector.exe -n darksiders1.exe -i ../target/debug/aether.dll
