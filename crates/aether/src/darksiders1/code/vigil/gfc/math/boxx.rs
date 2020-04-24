90c6d3e08ae424108473ca6ae18ac36fc3e162b5
John Simon <john@whatisaph.one>
2020-04-23 20:40:07 -0400

    Get rid of redundant bound

--- crates/aether/src/darksiders1/code/vigil/gfc/math/boxx.rs
+++ crates/aether/src/darksiders1/code/vigil/gfc/math/boxx.rs
@@ -1,16 +1,16 @@
 use darksiders1_sys::target;
-use na::{Point3, RealField, Scalar};
+use na::{Point3, RealField};
 
 #[repr(C)]
 #[derive(Clone)]
-pub struct TBox<N: Scalar + RealField> {
+pub struct TBox<N: RealField> {
     pub min: Point3<N>,
     pub max: Point3<N>,
 }
 
 impl_lift_lower!(TBox<f32>, target::gfc__TBox_float_gfc__FloatMath_);
 
-impl<N: Scalar + RealField> TBox<N> {
+impl<N: RealField> TBox<N> {
     pub fn new(min: Point3<N>, max: Point3<N>) -> Self {
         Self { min, max }
     }
